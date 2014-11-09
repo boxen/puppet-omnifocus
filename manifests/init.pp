2ece4143bc44c6f8abf872850117ff4041a2cb23
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2014-11-09 07:49:13 +0000

    Allow `omnifocus` to have `version` set
    This defaults to the current version of `1.10.4` to ensure backwards
    compatibility. Anyone who wants the current version 2.x should follow
    the README.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,9 +3,15 @@
 # Examples
 #
 #   include omnifocus
-class omnifocus {
+#
+# or with specific version
+#
+#   class { 'omnifocus':
+#     version => '2.0.3'
+#   }
+class omnifocus($version = '1.10.4') {
   package { 'OmniFocus':
     provider => 'appdmg_eula',
-    source   => 'http://www.omnigroup.com/ftp1/pub/software/MacOSX/10.6/OmniFocus-1.10.4.dmg'
+    source   => "http://www.omnigroup.com/ftp1/pub/software/MacOSX/10.9/OmniFocus-${version}.dmg"
   }
 }
